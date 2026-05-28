4fdd8bce9758ab3adc1c0806b8e99995e6426bae
Anderson <andersontopaga2018@gmail.com>
2026-05-28 16:49:18 -0500

    conexión backend con Supabase y configuración inicial

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,21 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { pool } from "./config/db";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Backend LUMI funcionando');
+app.get("/", async (req, res) => {
+  const resultado = await pool.query("SELECT NOW()");
+
+  res.json({
+    mensaje: "Conexión exitosa con Supabase",
+    fecha: resultado.rows[0],
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 3000');
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Servidor funcionando en http://localhost:3000");
 });
